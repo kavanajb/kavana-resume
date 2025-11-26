8b55d322d3f0f45a5150779e6eee7c0c5da99047
kavanajb <kavanakavi145@gmail.com>
2025-11-26 14:08:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let nameField = document.getElementById("name");
-    nameField.style.color = "blue";
-    nameField.style.fontWeight = "bold";
-});
