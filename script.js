703b35e456749eb925bf08e00489ecef894a62b8
kavanajb <kavanakavi145@gmail.com>
2025-11-26 14:10:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let nameField = document.getElementById("name");
+    nameField.style.color = "blue";
+    nameField.style.fontWeight = "bold";
+});
